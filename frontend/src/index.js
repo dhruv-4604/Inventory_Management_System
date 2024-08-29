9c7facaadf2d5e649e85e540721e271b69a1f37f
parth2564 <parthpatel25641517@gmail.com>
2024-08-29 20:59:58 +0530

    lil changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import SignInPage from "./Components/login/SignInPage";
-import SignUpPage from "./Components/login/SignUpPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <SignUpPage></SignUpPage> */}
   </React.StrictMode>
 );
